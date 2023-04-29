ef0822c0e857a8f52578213c1203e0e3f7a5138e
Crowdbotics <team@crowdbotics.com>
2023-04-29 18:12:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weathered_queen_40344/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weathered_queen_40344;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weathered_queen_40344";
+  }
+}
